4990fecfcd0719da1d156526560d13cd30386090
Ben Kolera <ben.kolera@gmail.com>
2011-10-05 18:17:40 +1000

    First Commit

---
+++ lib/Task/BeLike/BKOLERA.pm
@@ -0,0 +1,37 @@
+package Task::BeLike::BKOLERA;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Task::BeLike::BKOLERA - All of my modules I like to keep around.
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+Ben Kolera <<ben.kolera@gmail.com>>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Ben Kolera
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1; # End of Task::BeLike::BKOLERA
